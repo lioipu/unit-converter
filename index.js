75bf8cd72067ee63f3c2403c35fb99936ab31029
lioipu <57569699+lioipu@users.noreply.github.com>
2024-12-26 20:15:21 +0200

    second-commit

--- index.js
+++ index.js
@@ -3,19 +3,26 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
-let meter = 0
-let feet = 0
+
 let convertBtn = document.getElementById("convert-btn")
 convertBtn.addEventListener("click", convert)
 
 function convert() {
     let input = document.getElementById("input-number").value
-    meter = input
-    feet = meter * 3.281
-    document.getElementsByClassName("meters")[0].innerHTML = meter
-    document.getElementsByClassName("feet")[0].innerHTML = feet
-    document.getElementsByClassName("meters")[1].innerHTML = meter
-    document.getElementsByClassName("feet")[1].innerHTML = feet
+    document.getElementsByClassName("meters")[0].innerHTML = input
+    document.getElementsByClassName("feet")[0].innerHTML = ( input * 3.281 ).toFixed(3)
+    document.getElementsByClassName("feet")[1].innerHTML = input 
+    document.getElementsByClassName("meters")[1].innerHTML = ( input / 3.281 ).toFixed(3)
+
+    document.getElementsByClassName("liters")[0].innerHTML = input
+    document.getElementsByClassName("gallons")[0].innerHTML = ( input * 0.264 ).toFixed(3)
+    document.getElementsByClassName("gallons")[1].innerHTML = input
+    document.getElementsByClassName("liters")[1].innerHTML = ( input / 0.264 ).toFixed(3)
+
+    document.getElementsByClassName("kilos")[0].innerHTML = input
+    document.getElementsByClassName("pounds")[0].innerHTML = ( input * 2.204 ).toFixed(3)
+    document.getElementsByClassName("pounds")[1].innerHTML = input
+    document.getElementsByClassName("kilos")[1].innerHTML = ( input / 2.204 ).toFixed(3)
 
 }
 
